a784f86b7230fa24a7ffe43181581679ed1121a6
Jennifer-Blue-Williams <jennifer.blue.williams@gmail.com>
2021-12-13 14:50:57 -0600

    Created the satabase and mudeles for carsRus

---
+++ scripts/Orders.js
@@ -0,0 +1,41 @@
+import { getOrders, getMetals, getSizes, getStyles } from "./database.js"
+
+const orders = getOrders()
+const metals = getMetals()
+const styles = getStyles()
+const sizes = getSizes()
+
+
+const buildOrderListItem = (order) => { 
+    const foundMetal = metals.find((metal) => metal.id === order.metalId);
+    const foundStyle = styles.find((style) => style.id === order.styleId);
+    const foundSize = sizes.find((size) => size.id === order.sizeId);
+
+const totalCost = foundMetal.price + foundStyle.price + foundSize.price;
+
+const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+});
+    return `<li>
+    Order #${order.id} cost ${costString}
+</li>`
+}
+
+
+export const Orders = () => {
+    /*
+        Can you explain why the state variable has to be inside
+        the component function for Orders, but not the others?
+    */
+    const orders = getOrders();
+    let html = "<ul>"
+
+    const listItems = orders.map((order) => buildOrderListItem(order));
+
+    html += listItems.join("");
+    html += "</ul>";
+
+    return html;
+};
+
